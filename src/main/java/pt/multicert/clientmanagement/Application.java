2d13c7557b2fd71da14cd4e9294a9bc261c07445
Diogo Calado <dmcalado@gmail.com>
2019-01-07 19:04:08 +0000

    Web Services with SSL implemented!

--- src/main/java/pt/multicert/clientmanagement/Application.java
+++ src/main/java/pt/multicert/clientmanagement/Application.java
@@ -2,19 +2,12 @@ package pt.multicert.clientmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application  extends SpringBootServletInitializer {
+public class Application{
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(Application.class);
     }
 }
