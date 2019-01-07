4d4a531c63df08da927193095d7d911b9c243661
Diogo Calado <dmcalado@gmail.com>
2019-01-07 00:16:19 +0000

    Build Update

--- src/main/java/pt/multicert/clientmanagement/Application.java
+++ src/main/java/pt/multicert/clientmanagement/Application.java
@@ -2,15 +2,19 @@ package pt.multicert.clientmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import javax.swing.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
     }
 }
